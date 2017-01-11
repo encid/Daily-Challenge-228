a5b4440d99651b90743991117dba33f08dc05d8c
encid <ryancav@gmail.com>
2017-01-11 10:35:08 -0500

    commit

--- Daily-Challenge-228/Program.cs
+++ Daily-Challenge-228/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace DailyChallenge228
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var inputList = new List<string>{ "billowy", "biopsy", "chinos", "defaced", "chintz", "sponged",
+                                              "bijoux", "abhors", "fiddle", "begins", "chimps", "wronged" };
+
+            foreach (var item in inputList) {
+                Console.WriteLine(IsOrdered("sponged"));
+            }            
+
+            Console.ReadKey();            
+        }
+
+        static string IsOrdered(string input)
+        {
+            bool notOrdered = false;
+
+            // check if not ordered
+            for (int i = 0; i < input.Length - 1; i++) {
+                if (input[i] > input[i + 1]) {
+                    notOrdered = true;
+                    break;
+                }
+            }            
+
+            // since not ordered, check if in reverse order
+            if (notOrdered == true) {
+                for (int i = input.Length - 1; i > 0; i--) {    // iterate backwards from end of word
+                    if (input[i] > input[i - 1]) {     // if any char is higher than the one before it, string is not in order or reverse order
+                        return string.Format("{0} NOT IN ORDER", input); 
+                    }
+                }
+
+                // if code gets through the for loop, it's in reverse order
+                return string.Format("{0} REVERSE ORDER", input);
+            }
+
+            return string.Format("{0} IN ORDER", input);
         }
     }
 }
